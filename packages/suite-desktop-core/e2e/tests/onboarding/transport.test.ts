7ca599db12082fdde40fa742056e2fd74fd6b415
Ondřej Hájek <ondrej.hajek@satoshilabs.com>
2025-02-21 08:35:42 +0100

    feat(e2e): Finished onboarding tests migration (#16828)
    * feat(e2e): Finished onboarding tests migration
    
    * fix(e2e): Fixed review comments
    
    * fix(e2e): Fixed url in desktop test
    
    * fix(e2e): Fixed minor issues
    
    * fix(e2e): Fixed accounts url
    
    * fix(e2e): Skip bridge test
    
    * fix(e2e): gh token

---
+++ packages/suite-desktop-core/e2e/tests/onboarding/transport.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '../../support/fixtures';
+
+test.describe.skip(
+    'Onboarding - transport webusb/bridge',
+    { tag: ['@group=device-management', '@webOnly'] },
+    () => {
+        test.use({ startEmulator: false });
+
+        test.beforeEach(async ({ trezorUserEnvLink }) => {
+            await trezorUserEnvLink.stopBridge();
+            await trezorUserEnvLink.stopEmu();
+            await trezorUserEnvLink.startBridge();
+        });
+
+        test('Offer webusb as primary choice on web', async ({ page, analyticsPage }) => {
+            await analyticsPage.continueButton.click();
+            await expect(page.getByTestId('@webusb-button')).toBeVisible({ timeout: 30000 });
+            await page.getByTestId('@connect-device-prompt/no-device-detected').click();
+            await expect(page.getByTestId('@collapsible-box/body')).toHaveAttribute(
+                'aria-expanded',
+                'true',
+            );
+        });
+
+        test.afterEach(async ({ trezorUserEnvLink }) => {
+            await trezorUserEnvLink.stopBridge();
+        });
+    },
+);
