83cb5d2c7d11f1a3da2eb451f76bcbdc6ca7b72a
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2025-03-27 08:13:11 +0100

    Fix several skipped tests (#17854)
    condition retries only to CI
    removes few todos that we wont do
    refactors PW electron arguments
    enables EAP test
    fixes version test
    Fixes dry run Recovery with device reconnection
    Skip unstable metadata lifecycle test

--- packages/suite-desktop-core/e2e/support/electron.ts
+++ packages/suite-desktop-core/e2e/support/electron.ts
@@ -6,15 +6,19 @@ import { TrezorUserEnvLink } from '@trezor/trezor-user-env-link';
 
 // specific version of legacy bridge is requested & expected
 export const LEGACY_BRIDGE_VERSION = '2.0.33';
-const disableHashCheckStatePatch =
-    '--state.suite.settings.enabledSecurityChecks.firmwareHash=false';
-const disableRevisionCheckStatePatch =
+
+const appDir = path.join(__dirname, '../../../suite-desktop');
+const disableHashChecksPatch = '--state.suite.settings.enabledSecurityChecks.firmwareHash=false';
+const disableFirmwareRevisionChecksPatch =
     '--state.suite.settings.enabledSecurityChecks.firmwareRevision=false';
-const showDebugMenuStatePatch = `--state.suite.settings.debug.showDebugMenu=true`;
+const showDebugMenuStatePatch = '--state.suite.settings.debug.showDebugMenu=true';
+// #15670 Bug in desktop app that loglevel is ignored
+const logLevelArgument = `--log-level=${process.env.LOGLEVEL ?? 'debug'}`;
+const disableHWAccelerationArgument = '--disable-gpu'; // to fix chromium error GetVSyncParametersIfAvailable()
+const removeUserDataArgument = '--remove-user-data-on-start';
 
 type LaunchSuiteParams = {
-    rmUserData?: boolean;
-    bridgeLegacyTest?: boolean;
+    keepUserData?: boolean;
     bridgeDaemon?: boolean;
     locale?: string;
     colorScheme?: 'light' | 'dark' | 'no-preference' | null | undefined;
@@ -37,44 +41,38 @@ const formatErrorLogMessage = (data: string) => {
     return `${timestamp} - ${bold}${red}ERROR${unbold}: ${data}${reset}`;
 };
 
-export const launchSuiteElectronApp = async (params: LaunchSuiteParams) => {
-    const defaultParams = {
-        rmUserData: true,
-        bridgeLegacyTest: true,
-        bridgeDaemon: false,
-    };
-    const options = Object.assign(defaultParams, params);
+const buildArgs = (params: LaunchSuiteParams) => {
+    const args = [
+        path.join(appDir, './dist/app.js'),
+        `--width=${params.viewport.width}`,
+        `--height=${params.viewport.height}`,
+        logLevelArgument,
+        disableHWAccelerationArgument,
+        disableHashChecksPatch,
+        showDebugMenuStatePatch,
+    ];
 
-    const appDir = path.join(__dirname, '../../../suite-desktop');
-    // #15670 Bug in desktop app that loglevel is ignored
-    const logLevelArgument = `--log-level=${process.env.LOGLEVEL ?? 'debug'}`;
-    const viewportArgument = `--width=${options.viewport.width} --height=${options.viewport.height}`;
-    const disableHWAccelerationArgument = '--disable-gpu'; // to fix chromium error GetVSyncParametersIfAvailable()
-    if (!options.bridgeDaemon) {
-        // TODO: #15646 Find out why currently pw fails to see node-bridge so we default to legacy bridge.
-        await TrezorUserEnvLink.startBridge(LEGACY_BRIDGE_VERSION);
+    if (params.bridgeDaemon) {
+        args.push('--bridge-daemon', '--skip-new-bridge-rollout');
+    } else {
+        args.push('--bridge-legacy', '--bridge-test');
     }
-    const electronApp = await electron.launch({
-        cwd: appDir,
-        args: [
-            path.join(appDir, './dist/app.js'),
-            viewportArgument,
-            logLevelArgument,
-            disableHWAccelerationArgument,
-            ...(options.bridgeLegacyTest ? ['--bridge-legacy', '--bridge-test'] : []),
-            ...(options.bridgeDaemon ? ['--bridge-daemon', '--skip-new-bridge-rollout'] : []),
-            ...(options.rmUserData ? ['--remove-user-data-on-start'] : []),
-            disableHashCheckStatePatch,
-            process.env.CANARY_FIRMWARE ? disableRevisionCheckStatePatch : '',
-            showDebugMenuStatePatch,
-        ],
-        colorScheme: params.colorScheme,
-        locale: params.locale,
-        recordVideo: { dir: options.artefactFolder, size: options.viewport },
-    });
 
-    const logFilePath = path.join(options.artefactFolder, 'electron-logs.txt');
-    ensureDirSync(options.artefactFolder);
+    const deleteUserData = !params.keepUserData;
+    if (deleteUserData) {
+        args.push(removeUserDataArgument);
+    }
+
+    if (process.env.CANARY_FIRMWARE) {
+        args.push(disableFirmwareRevisionChecksPatch);
+    }
+
+    return args;
+};
+
+const setupLoggingToFile = (electronApp: ElectronApplication, params: LaunchSuiteParams) => {
+    const logFilePath = path.join(params.artefactFolder, 'electron-logs.txt');
+    ensureDirSync(params.artefactFolder);
     const logStream = createWriteStream(logFilePath, { flags: 'a' });
 
     electronApp.process().stdout?.on('data', data => logStream.write(data.toString()));
@@ -84,6 +82,27 @@ export const launchSuiteElectronApp = async (params: LaunchSuiteParams) => {
     electronApp.process().on('close', () => {
         logStream.end();
     });
+};
+
+export const launchSuiteElectronApp = async (params: LaunchSuiteParams) => {
+    if (!params.bridgeDaemon) {
+        // TODO: #15646 Find out why currently pw fails to see node-bridge so we default to legacy bridge.
+        await TrezorUserEnvLink.startBridge(LEGACY_BRIDGE_VERSION);
+    }
+
+    const electronApp = await electron.launch({
+        cwd: appDir,
+        args: buildArgs(params),
+        env: {
+            ...process.env,
+            PLAYWRIGHT_RUN: 'true',
+        },
+        colorScheme: params.colorScheme,
+        locale: params.locale,
+        recordVideo: { dir: params.artefactFolder, size: params.viewport },
+    });
+
+    setupLoggingToFile(electronApp, params);
 
     return electronApp;
 };
--- packages/suite-desktop-core/e2e/tests/general/eap-modal.test.ts
+++ packages/suite-desktop-core/e2e/tests/general/eap-modal.test.ts
@@ -1,23 +1,13 @@
 import { expect, test } from '../../support/fixtures';
 
-/**
- * Test case:
- * 1. Navigate to `Settings/Application`
- * 2. Scroll down to the `Experimental features` part
- * 3. Click on Join button
- * 4. Confrim the EAP modal
- * 5. Check if there is a button with `Leave` on it
- */
-// TODO: #15561 FIX settings cleanup: eap setting is remembered even after cache cleanup at the beginning of the test. This shouldn't affect gha run but breaks the local one.
-test.skip(!process.env.GITHUB_ACTION, 'Test is working only in CI. Skipping local run.');
 test.use({ startEmulator: false });
-test.beforeAll(async ({ onboardingPage }) => {
-    await onboardingPage.completeOnboarding();
-});
 
-test('Join early access button', { tag: '@settings' }, async ({ settingsPage }) => {
-    const buttonText = 'Leave';
-    await settingsPage.navigateTo('application');
-    await settingsPage.joinEarlyAccessProgram();
-    expect(await settingsPage.earlyAccessJoinButton.textContent()).toContain(buttonText);
-});
+test(
+    'Join early access',
+    { tag: ['@group=settings', '@desktopOnly'] },
+    async ({ settingsPage }) => {
+        await settingsPage.navigateTo('application');
+        await settingsPage.joinEarlyAccessProgram();
+        await expect(settingsPage.earlyAccessJoinButton).toHaveText('Opt out');
+    },
+);
